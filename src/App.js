0d0334135e64efc218eb1479f5675139b02bdd59
FhatimaReyes <fhareal10@gmail.com>
2023-06-02 14:26:55 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Isesion from './components/Isesion.jsx';
+import Administrador from './components/Administrador.jsx';
+import Sucursal from './components/Sucursal.jsx';
+import Casa from './components/Casa.jsx'
+import Almacenamiento from './components/Almacenamiento';
+
+function App() {
+  return (
+    <div className="App">
+       <Isesion /> 
+       <Administrador /> 
+       <Sucursal /> 
+       <Casa /> 
+      <Almacenamiento />
+    </div>
+  );
+}
+
+export default App;
