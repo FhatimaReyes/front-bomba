c8dae717fbc9177d275acf8b98f478835747f35e
YareliB07 <yarelibravo84@gmail.com>
2023-06-02 21:47:39 -0600

    update front code

--- src/components/Casa.jsx
+++ src/components/Casa.jsx
@@ -1,9 +1,71 @@
-import React from "react";
+//import React from "react";
+import React, {useState} from "react";
 import Componente from "./Componente";
 import Portada from "./Portada";
 import './Casa.css'
+//import { useLocation} from "react-router-dom";
+import Almacenamiento from "./Almacenamiento";
+import { useLocation, useNavigate} from "react-router-dom";
+import axios from "axios";
+
+const apiurl = "https://fastapi-juandavid1217.cloud.okteto.net/"
 
 function Casa () {
+    const location = useLocation();
+    console.log(location.state);
+    const navegar=useNavigate();
+    const casa=location.state;
+    const opcion=2;
+    const[capacidad, setCapacidad]=useState(0);
+    const[ubicacion, setUbicacion]=useState('');
+
+    const ChangeCapacidad=(e)=>{
+        setCapacidad({capacidad:e.target.value})
+    }
+
+    const ChangeUbicacion=(e)=>{
+        setUbicacion({ubicacion:e.target.value})
+    }
+
+    const createAlma=(e, capacidad, ubicacion, id_grupo, grupos)=>{
+        e.preventDefault();
+        addAlma(e, capacidad, ubicacion, id_grupo, grupos);
+    }
+    const addAlma=(e, capacidad, ubicacion, id_grupo, grupos)=>{
+        e.preventDefault();
+        axios(
+            {
+                method: 'POST',
+                url: apiurl+"Administrador-Casa/Almacenamiento/",
+                data:{
+                    capacidad_maxima: capacidad,
+                    ubicacion:ubicacion,
+                    id_grupo: id_grupo
+                }
+            }
+        ).then(res=>{
+            console.log("Respuesta de guardado (Almacenamiento): "+res.status)
+            getAlmas(e, id_grupo, grupos)
+        }).catch(errors=>{
+        })
+    }
+
+    const getAlmas=(e, id_grupo, grupos)=>{
+        e.preventDefault();
+        axios(
+            {
+                method: 'GET',
+                url: apiurl+"Administrador-Casa/Almacenamientos/"+id_grupo
+            }
+        ).then(res=>{
+            if(res.status==200){
+                grupos['grupos'][0]['almacenamientos']=res.data;
+                navegar('/Casa', {state:grupos})
+            }
+        }).catch(errors=>{
+        })
+    }
+//{casa['nombre']==null?(casa['grupos'][0]['nombre']):(casa['nombre'])}
     return (
         <div className="mainCasa">
             <Portada
@@ -11,15 +73,20 @@ function Casa () {
             />
              <div className="contenidoCasa">
                 <div className="gpoCasa">
-                    <h1>Mis Grupos</h1>
+                    <h1>{casa['grupos'][0]['nombre']}</h1>
                     <div className="noRegistros">
                         <p>No. Máximo de Registros:</p>
                         <p>5</p>
                         <p>Registros Actuales:</p>
-                        <p>5</p>
+                        <p>{casa['grupos'][0]['almacenamientos'].length}</p>
                     </div>
                     <div className="gpoTinacos">
-                        <button className="nuevoTinaco">
+                        {casa['grupos'][0]['almacenamientos'].map((alm, index)=>(
+                            
+                            <div key={index}><Componente nombre={alm['ubicacion']} group_id={alm['id_almacenamiento']} navegar={opcion}/></div>
+                            
+                        ))}
+                        {/*<button className="nuevoTinaco">
                             <box-icon name='folder-plus' color='#456c75' ></box-icon>
                             <p>Crear</p>
                         </button>
@@ -37,21 +104,21 @@ function Casa () {
                         />
                         <Componente
                             nombre = "Alberca"
-                        />
+                        />*/}
                     </div>
                 </div>
                 <div className="altaAlmacenamiento">
                     <h2>Alta de Almacenamiento</h2>
                     <form action="">
-                        <label htmlFor="nombreAlmacenamiento">Nombre</label>
-                        <input type="text" id="nombreAlmacenamiento" />
+                        {/*<label htmlFor="nombreAlmacenamiento">Nombre</label>
+                        <input type="text" id="nombreAlmacenamiento" />*/}
                         <label htmlFor="capacidadMax">Capacidad Máxima</label>
-                        <input type="number" id="capacidadax" />
+                        <input type="number" id="capacidadax" onChange={(e)=>{ChangeCapacidad(e)}}/>
                         <label htmlFor="ubicacionAlmacenamiento">Ubicación</label>
-                        <input type="text" id="ubicacionAlmacenamiento" />
+                        <input type="text" id="ubicacionAlmacenamiento" onChange={(e)=>{ChangeUbicacion(e)}}/>
                     </form>
                     <div className="botonesAlta">
-                        <button>Crear</button>
+                        <button onClick={(e)=>{createAlma(e, capacidad['capacidad'], ubicacion['ubicacion'], casa['grupos'][0]['id_grupo'], casa)}} disabled={casa['grupos'][0]['almacenamientos'].length>=5?(true):(false)}>Crear</button>
                     </div>
                 </div>
             </div>
--- src/components/Sucursal.jsx
+++ src/components/Sucursal.jsx
@@ -1,9 +1,69 @@
-import React from "react";
+import React, {useState} from "react";
 import Portada from "./Portada";
 import Componente from "./Componente";
 import './Sucursal.css';
+import { useLocation, useNavigate} from "react-router-dom";
+import axios from "axios";
 
-function Sucursal() {
+const apiurl = "https://fastapi-juandavid1217.cloud.okteto.net/"//"https://fastapi-juandavid1217.cloud.okteto.net/"
+
+function Sucursal(props) {
+    const location = useLocation();
+    const navegar=useNavigate();
+    console.log(location.state);
+    const grupos=location.state;
+    const opcion=2;
+    const {tipo}=props;
+    const[capacidad, setCapacidad]=useState(0);
+    const[ubicacion, setUbicacion]=useState('');
+
+    const ChangeCapacidad=(e)=>{
+        setCapacidad({capacidad:e.target.value})
+    }
+
+    const ChangeUbicacion=(e)=>{
+        setUbicacion({ubicacion:e.target.value})
+    }
+
+    const createAlma=(e, capacidad, ubicacion, id_grupo, grupos)=>{
+        e.preventDefault();
+        addAlma(e, capacidad, ubicacion, id_grupo, grupos);
+    }
+    const addAlma=(e, capacidad, ubicacion, id_grupo, grupos)=>{
+        e.preventDefault();
+        axios(
+            {
+                method: 'POST',
+                url: apiurl+"Administrador-Casa/Almacenamiento/",
+                data:{
+                    capacidad_maxima: capacidad,
+                    ubicacion:ubicacion,
+                    id_grupo: id_grupo
+                }
+            }
+        ).then(res=>{
+            console.log("Respuesta de guardado (Almacenamiento): "+res.status)
+            getAlmas(e, id_grupo, grupos)
+        }).catch(errors=>{
+        })
+    }
+
+    const getAlmas=(e, id_grupo, grupos)=>{
+        e.preventDefault();
+        axios(
+            {
+                method: 'GET',
+                url: apiurl+"Administrador-Casa/Almacenamientos/"+id_grupo
+            }
+        ).then(res=>{
+            if(res.status==200){
+                grupos['almacenamientos']=res.data;
+                navegar('/Admin/Grupos', {state:grupos})
+            }
+        }).catch(errors=>{
+        })
+    }
+
     return (
         <div className="mainSucursal">
             <Portada
@@ -11,15 +71,20 @@ function Sucursal() {
             />
             <div className="contenidoSucursal">
                 <div className="gpoSucursal">
-                    <h1>Sucursal Córdoba</h1>
+                    <h1>{grupos['nombre']}</h1>
                     <div className="noRegistros">
                         <p>No. Máximo de Registros:</p>
                         <p>5</p>
                         <p>Registros Actuales:</p>
-                        <p>5</p>
+                        <p>{grupos['almacenamientos'].length}</p>
                     </div>
                     <div className="gpoAlmacenamientos">
-                        <button className="nuevoAlmacenamiento">
+                        {grupos['almacenamientos'].map((alm, index)=>(
+                            
+                            <div key={index}><Componente nombre={alm['ubicacion']} group_id={alm['id_almacenamiento']} navegar={opcion}/></div>
+                            
+                        ))}
+                        {/*<button className="nuevoAlmacenamiento">
                             <box-icon name='folder-plus' color='#456c75' ></box-icon>
                             <p>Crear</p>
                         </button>
@@ -37,23 +102,24 @@ function Sucursal() {
                         />
                         <Componente
                             nombre="Tinaco Cinco"
-                        />
+                        />*/}
                     </div>
                 </div>
+                {tipo==2?(
                 <div className="altaAlmacenamiento">
                     <h2>Alta de Almacenamiento</h2>
                     <form action="">
-                        <label htmlFor="nombreAlmacenamiento">Nombre</label>
-                        <input type="text" id="nombreAlmacenamiento" />
+                        {/*<label htmlFor="nombreAlmacenamiento">Nombre</label>
+                        <input type="text" id="nombreAlmacenamiento" />*/}
                         <label htmlFor="capacidadMax">Capacidad Máxima</label>
-                        <input type="number" id="capacidadax" />
+                        <input type="number" id="capacidadax" onChange={(e)=>{ChangeCapacidad(e)}}/>
                         <label htmlFor="ubicacionAlmacenamiento">Ubicación</label>
-                        <input type="text" id="ubicacionAlmacenamiento" />
+                        <input type="text" id="ubicacionAlmacenamiento" onChange={(e)=>{ChangeUbicacion(e)}}/>
                     </form>
                     <div className="botonesAlta">
-                        <button>Crear</button>
+                        <button onClick={(e)=>{createAlma(e, capacidad['capacidad'], ubicacion['ubicacion'], grupos['id_grupo'], grupos)}} disabled={grupos['almacenamientos'].length>=5?(true):(false)}>Crear</button>
                     </div>
-                </div>
+                </div>):(<></>)}
             </div>
         </div>
     );
